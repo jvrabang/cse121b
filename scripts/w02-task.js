d73bdfb8b0db9118364bf4255dcdfb417908b2da
jvrabang <jvrabang@gmail.com>
2023-09-23 22:12:02 +0800

    W02 Programming Tasks

---
+++ scripts/w02-task.js
@@ -0,0 +1,31 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullname = "John Vincent Rabang";
+let currentYear = 2023
+let profilePicture = "images/jv.png"
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+var imageElement = document.querySelector('img');
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullname}</strong>`
+yearElement.textContent = currentYear
+imageElement.setAttribute('src', profilePicture)
+imageElement.setAttribute('alt', `Profile image of ${fullname}`)
+
+/* Step 5 - Array */
+let favoriteFood = ['Shrimp', 'Ramen', 'Kimchi', 'Avocado Ice Cream'];
+foodElement.textContent = `${favoriteFood}`;
+let newfavoriteFood = 'Mango';
+favoriteFood.push(newfavoriteFood);
+foodElement.innerHTML += `<br>${favoriteFood}`;
+favoriteFood.shift();
+foodElement.innerHTML += `<br>${favoriteFood}`;
+favoriteFood.pop();
+foodElement.innerHTML += `<br>${favoriteFood}`;
